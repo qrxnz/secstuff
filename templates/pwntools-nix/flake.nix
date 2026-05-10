abba20a3e33f01a5bbb06e71d7390ef7303ac186
Karol Skoczyk <send@qrxnz.dev>
2026-05-10 12:25:32 +0200

    Add pwninit to pwntools Nix template on Linux

--- templates/pwntools-nix/flake.nix
+++ templates/pwntools-nix/flake.nix
@@ -18,19 +18,23 @@
       devShells.default = pkgs.mkShell {
         name = "python-venv";
         venvDir = "./.venv";
-        buildInputs = [
-          # A Python interpreter including the 'venv' module is required to bootstrap
-          # the environment.
-          pythonPackages.python
+        buildInputs =
+          [
+            # A Python interpreter including the 'venv' module is required to bootstrap
+            # the environment.
+            pythonPackages.python
 
-          # This executes some shell code to initialize a venv in $venvDir before
-          # dropping into the shell
-          pythonPackages.venvShellHook
+            # This executes some shell code to initialize a venv in $venvDir before
+            # dropping into the shell
+            pythonPackages.venvShellHook
 
-          # Those are dependencies that we would like to use from nixpkgs, which will
-          # add them to PYTHONPATH and thus make them accessible from within the venv.
-          pythonPackages.pwntools
-        ];
+            # Those are dependencies that we would like to use from nixpkgs, which will
+            # add them to PYTHONPATH and thus make them accessible from within the venv.
+            pythonPackages.pwntools
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+            pkgs.pwninit
+          ];
 
         # Run this command, only after creating the virtual environment
         postVenvCreation = ''
